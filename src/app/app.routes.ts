d87a86704975e245cea528f7d36a11663f138df4
dw42CSCE <dw42@email.sc.edu>
2025-07-09 19:39:57 -0400

    Fixing Reload issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { WineDashboard } from './wine-dashboard/wine-dashboard';
 import { WineDescription } from './wine-description/wine-description';
 import { Login } from './login/login';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/wine-dashboard', pathMatch: 'full' },
-    { path: 'wine-dashboard', component: WineDashboard },
-    { path: 'wine/:id', component: WineDescription },
-    { path: 'login', component: Login},
+  { path: '', redirectTo: '/wine-dashboard', pathMatch: 'full' },
+  { path: 'wine-dashboard', component: WineDashboard },
+  { path: 'wine/:id', component: WineDescription },
+  { path: 'login', component: Login },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
