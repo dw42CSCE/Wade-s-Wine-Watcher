c6b594cc1fb9e52eed2d0f73ecf22301befee536
dw42CSCE <dw42@email.sc.edu>
2025-07-21 14:57:03 -0400

    pushing

--- src/app/newwine/newwine.spec.ts
+++ src/app/newwine/newwine.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Newwine } from './newwine';
+import { NewWine } from './newwine';
 
 describe('Newwine', () => {
-  let component: Newwine;
-  let fixture: ComponentFixture<Newwine>;
+  let component: NewWine;
+  let fixture: ComponentFixture<NewWine>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Newwine]
+      imports: [NewWine]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Newwine);
+    fixture = TestBed.createComponent(NewWine);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
